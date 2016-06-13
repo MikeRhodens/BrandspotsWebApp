8ed314e5d285c3d97cf7613e4ef0973204d7eb4b
19392 <19392@ma-web.nl>
2016-06-13 14:39:31 +0200

    ''

--- views/overzicht.php
+++ views/overzicht.php
@@ -1,12 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>App</title>
-	<link rel="stylesheet" href="assets/css/style.css">
-	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-</head>
 <body>
 	<div id="container-app">
 		<div class="cta-button">
@@ -105,26 +98,7 @@
 			<div class="clear"></div>
 		</div><!--End product-->
 
-		<footer>
-			<div class="nav-footer">
-				<ul>
-					<li><a href="#" class="nav-active"><i class="fa fa-eye" aria-hidden="true"></i></a></li>
-					<li><a href="cart.html"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a></li>
-					<li><a class="search"><i class="fa fa-search" aria-hidden="true"></i></a></li>
-				</ul>
-			</div><!--End nav-footer-->
-				<div class="input"><!--Deze div komt omhoog als je search klikt-->
-					<div>
-    						<input type="text" class="search-input" value="" placeholder="Search..." name="text" id="icon-search">
-    						<button class="search-button"><i class="fa fa-search"></i></button>
-    					</div>
-				</div>
-			<div class="clear"></div>
-		</footer>
 	</div><!--End container-app-->
 
-	<script src="libs/jquery.js"></script>
-	<script src="assets/js/index.js"></script>
 
-</body>
-</html>
+
