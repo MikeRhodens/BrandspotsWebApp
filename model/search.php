faa62575b2b5858929d53657aa82f3aaf008e875
19392 <19392@ma-web.nl>
2016-06-14 16:14:46 +0200

    search secured against sql injection

--- model/search.php
+++ model/search.php
@@ -2,6 +2,7 @@
 
 $searchQuery = isset($_POST['searchQuery']) ? $_POST['searchQuery'] : "" ;
 $searchQuery = "%" . $searchQuery . "%" ;
+$searchQuery = mysqli_real_escape_string($mysqli,$searchQuery);
 $query = "SELECT * FROM products WHERE product_name LIKE '$searchQuery'";
 $result = $mysqli->query($query);
 // var_dump($result);
