af8336a21bf9d94393a70c048c8913c0bec49517
Adrienne van Gijzen <tristanvdl@gmail.com>
2016-06-15 12:32:18 +0200

    added more session code for the cart

--- model/cart.php
+++ model/cart.php
@@ -12,8 +12,21 @@ if ($cart_product_id != "") {
     echo "lmao";
 }
 
-var_dump($_SESSION['cart_id']);
-echo $cart_product_id;
+$result_cart = [];
+$sql_cart_query = "SELECT * FROM products WHERE";
+$counter = 1;
+foreach ($_SESSION['cart_id'] as $id) {
+    if ($counter > 1) {
+        $sql_cart .= " OR ";
+    }
+     $sql_cart .= " id = $id";
+     $counter .= 1;
+}
+
+$sql_cart_query = mysqli_real_escape_string($mysqli, $sql_cart_query);
+$result_cart = $mysqli->query($query);
+
+
 
 
  ?>
