3b62e7832d0ce638d07832beccbf1bfd3e87effb
Adrienne van Gijzen <tristanvdl@gmail.com>
2016-06-15 12:41:00 +0200

    cart almost done

--- model/cartModel.php
+++ model/cartModel.php
@@ -17,14 +17,22 @@ $sql_cart_query = "SELECT * FROM products WHERE";
 $counter = 1;
 foreach ($_SESSION['cart_id'] as $id) {
     if ($counter > 1) {
-        $sql_cart .= " OR ";
+        $sql_cart_query .= " OR ";
     }
-     $sql_cart .= " id = $id";
+     $sql_cart_query .= " id = $id";
      $counter .= 1;
 }
 
 $sql_cart_query = mysqli_real_escape_string($mysqli, $sql_cart_query);
-$result_cart = $mysqli->query($query);
+$result_cart = $mysqli->query($sql_cart_query);
+
+foreach($result_cart as $row){
+  $name = $row['product_name'];
+  $price = $row['product_price'];
+  $desc = $row['product_desc'];
+  $img = $row['product_image'];
+  $id = $row['id'];
+}
 
 
 
--- views/cart.php
+++ views/cart.php
@@ -3,7 +3,30 @@
 </div>
 
 <div class="main-cart">
+    <div class="product-main">
+        <div class="image-main">
+            <img src="assets/links/<?php echo $img; ?>.JPG" alt="backpack">
+        </div>
+        <div class="info-product">
+            <div class="title-product">
+                <h1><?php echo $name; ?></h1>
+            </div>
+            <div class="price-product">
+                <h4><span class="bold">Prijs:</span> &euro; <?php echo $price; ?> </h4>
+            </div>
+            <div class="cta-button">
+                <a href="?action=cart&id=<?php echo $id; ?>" style="height:0px;">
+                    <button class="cart-button">Add to cart</button
+                </a>
+            </div>
 
+            <div class="product-description">
+                <?php echo $desc; ?>
+
+            </div>
+
+        </div><!--End info-product-->
+    </div>
 </div>
 
 <div class="info">
