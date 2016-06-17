c0234f7eedf791e791e3d7bf523819be5d0a7a37
tristan van der linden <tristan van der linden>
2016-06-17 13:39:29 +0200

    fixed cart bug

--- model/cartModel.php
+++ model/cartModel.php
@@ -10,24 +10,25 @@ if ($cart_product_id != "") {
     if(!in_array($cart_product_id,$_SESSION['cart_id'])){
         $_SESSION['cart_id'][] = $cart_product_id;
     }
-}else {
-
 }
 
 $_SESSION['cart_id'] = array_diff($_SESSION['cart_id'], array($remove_cart_id));
-
 $result_cart = [];
-$sql_cart_query = "SELECT * FROM products WHERE";
-$counter = 1;
-foreach ($_SESSION['cart_id'] as $id) {
-    if ($counter > 1) {
-        $sql_cart_query .= " OR ";
-    }
-     $sql_cart_query .= " id = $id";
-     $counter .= 1;
+
+if ($_SESSION['cart_id'] != null) {
+  $sql_cart_query = "SELECT * FROM products WHERE";
+  $counter = 1;
+  foreach ($_SESSION['cart_id'] as $id) {
+      if ($counter > 1) {
+          $sql_cart_query .= " OR ";
+      }
+       $sql_cart_query .= " id = $id";
+       $counter .= 1;
+  }
+  $sql_cart_query = mysqli_real_escape_string($mysqli, $sql_cart_query);
+  $result_cart = $mysqli->query($sql_cart_query);
 }
 
-$sql_cart_query = mysqli_real_escape_string($mysqli, $sql_cart_query);
-$result_cart = $mysqli->query($sql_cart_query);
+
 
  ?>
